1bb73ebade5027bc41360e1175952ab6f364fc07
Impereal <an2an96@bk.ru>
2014-09-17 21:50:43 +0400

    update

--- App2/App2/App2.cpp
+++ App2/App2/App2.cpp
@@ -6,27 +6,25 @@
 
 using namespace std;
 
+bool Check(float a1, float b1, float r1, float a2, float b2, float r2)
+{
+	if ((a1 + r1) >= (a2 + r2) && (a1 - r1) <= (a2 - r2) && (b1 + r1) >= (b2 + r2) && (b1 - r1) <= (b2 - r2))
+		return true;
+	else
+		return false;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL, "Russian");
 
-	int a1, b1, r1, a2, b2, r2;
-
+	float a1, b1, r1, a2, b2, r2;
 	cout << "Введите a1, b1, r1, a2, b2, r2: ";
 	cin >> a1 >> b1 >> r1 >> a2 >> b2 >> r2;
 	
-	if ((a1 + r1) >= (a2 + r2) && (a1 - r1) <= (a2 - r2) && (b1 + r1) >= (b2 + r2) && (b1 - r1) <= (b2 - r2))
-	{
-		cout << "Окружность 2 лежит в окружности 1!" << endl;
-	}
-	else if ((a1 + r1) <= (a2 + r2) && (a1 - r1) >= (a2 - r2) && (b1 + r1) <= (b2 + r2) && (b1 - r1) >= (b2 - r2))
-	{
-		cout << "Окружность 1 лежит в окружности 2!" << endl;
-	}
-	else
-	{
-		cout << "Ни одна из окружностей не лежит друг в друге!" << endl;
-	}
+	if (Check(a1, b1, r1, a2, b2, r2))			cout << "Окружность 2 лежит в окружности 1!" << endl;
+	else if (Check(a2, b2, r2, a1, b1, r1))		cout << "Окружность 1 лежит в окружности 2!" << endl;
+	else										cout << "Ни одна из окружностей не лежит друг в друге!" << endl;
 	system("pause");
 	return 0;
 }
