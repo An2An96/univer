bee0aa8cd32313f4970349841332643b38a826a5
Impereal <an2an96@bk.ru>
2014-09-21 14:05:12 +0400

    update

--- App3/App3.cpp
+++ App3/App3.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void IntToPrime(int num)
 {
+
 	int pnumbers[] = {
 		2, 3, 5, 7, 11, 13, 17,
 		19, 23, 29, 31, 37, 41, 43,
@@ -16,6 +17,7 @@ void IntToPrime(int num)
 	};
 
 	cout << num << " = ";
+	bool error = true;
 	while (num > 1)
 	{
 		for (int i = 0; i < sizeof pnumbers; i++)
@@ -26,8 +28,15 @@ void IntToPrime(int num)
 			cout << pnumbers[i];
 			if (num != 1)
 				cout << " * ";
+			error = false;
 			break;
 		}
+		if (error)
+		{
+			cout << "Не получается:C";
+			break;
+		}
+		error = true;
 	}
 	cout << endl;
 }
@@ -37,11 +46,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	setlocale(LC_ALL, "Russian");
 
 	int number;
-	cout << "Введите целое число:" << endl;
-	cin >> number;
+	do
+	{
+		cout << "Введите целое число:" << endl;
+		cin >> number;
+	} while (number < 1);
 
 	IntToPrime(number);
-
 	system("pause");
-	return 0;
+	return false;
 }
