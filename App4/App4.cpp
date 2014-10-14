7cafc3525a8e3c931c288363e75baab5bfab48c4
An2An <an2an96@bk.ru>
2014-10-14 18:26:42 +0400

    change gen_random and optimize

--- App4/App4.cpp
+++ App4/App4.cpp
@@ -6,27 +6,30 @@
 
 #define	M	3
 #define	N	3
-#define gen_random(_range_min, _range_max)	\
-	(_range_min + rand() % (_range_max - _range_min + 1))
+
+int gen_random(int range_min, int range_max)	{
+	return (range_min + rand() % (range_max - range_min + 1));
+}
 
 int _tmain(int argc, _TCHAR* argv[])
-{	
+{
 	//	init
 	srand(time(NULL));
 	//	initialize array
-	int A[M][N];
-	for (int m = 0; m < M; m++)
-		for (int n = 0; n < N; n++)
+	int A[M][N], m = 0, n = 0;
+	for (; m < M; m++)
+		for (n = 0; n < N; n++)
 			A[m][n] = gen_random(1, 9);
-	//	perform		
-	for (int m = 0; m < M; m++)	{
-		for (int n = 0; n < N; n++)	{
-			if (m - 1 >= 0)
-				A[m][n] += (n - 1 >= 0)	? (A[m - 1][n] - A[m - 1][n - 1] + A[m][n - 1]) : (A[m - 1][n]);
-			else
-				A[m][n] += (n - 1 >= 0) ? (A[m][n - 1]) : (0);
-		} 
-	}
+	//	perform
+	for (n = 1; n < N; n++)	
+		A[0][n] += A[0][n - 1];
+
+	for (m = 1; m < M; m++)
+		A[m][0] += A[m - 1][0];
+
+	for (m = 1; m < M; m++)
+		for (n = 1; n < N; n++)
+			A[m][n] += A[m - 1][n] - A[m - 1][n - 1] + A[m][n - 1];
 	system("pause");
 	return 0;
 }
