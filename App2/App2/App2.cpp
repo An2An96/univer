550d6ec219cf49263c8fcd9163a6aede46b20c34
An2An <an2an96@bk.ru>
2014-10-16 20:16:13 +0400

    Update App2.cpp

--- App2/App2/App2.cpp
+++ App2/App2/App2.cpp
@@ -1,6 +1,5 @@
 // Проверить, лежит ли окружность (x-a1)^2+(y-b1)^2=r^2 целиком внутри окружности (x-a2)^2+(y-b2)^2=r^2 или наоборот
-
-//	Nemirov Anton	(13501/3)	<an2an96@bk.ru>
+// Nemirov Anton 13501/3
 
 #include "stdafx.h"
 
@@ -22,9 +21,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Введите a1, b1, r1, a2, b2, r2: ";
 	cin >> a1 >> b1 >> r1 >> a2 >> b2 >> r2;
 	
-	if (Check(a1, b1, r1, a2, b2, r2))			cout << "Окружность 2 лежит в окружности 1!" << endl;
-	else if (Check(a2, b2, r2, a1, b1, r1))		cout << "Окружность 1 лежит в окружности 2!" << endl;
-	else										cout << "Ни одна из окружностей не лежит друг в друге!" << endl;
+	if (Check(a1, b1, r1, a2, b2, r2))	cout << "Окружность 2 лежит в окружности 1!" << endl;
+	else if (Check(a2, b2, r2, a1, b1, r1))	cout << "Окружность 1 лежит в окружности 2!" << endl;
+	else					cout << "Ни одна из окружностей не лежит друг в друге!" << endl;
 	system("pause");
 	return 0;
 }
