8fb3552147979f476c50b7af0b2db092f096ef9b
An2An <an2an96@bk.ru>
2014-10-19 11:43:04 +0400

    Update App5.cpp

--- App5/App5.cpp
+++ App5/App5.cpp
@@ -10,15 +10,13 @@ void replace(string &s)
 {	
 	//tr1::regex rx("/ ^\w*(#\w*)\s\, \.");
 	//tr1::regex_replace(s, rx, "");
-	int n, m, tmp;
-	do
-	{
-		tmp = 0;
+	int n = 0, m;
+	for (; n != -1;)
 		if ((n = s.find("$", 0)) != -1)
-		{
 			s.replace(n, 1, "\t");
-			tmp = 1;
-		}
+
+	for (n = 0; n != -1;)
+	{
 		if ((n = s.find("#", 0)) != -1)
 		{
 			if ((m = s.find(" ", n)) != -1)
@@ -29,21 +27,19 @@ void replace(string &s)
 				s.erase(n, m - n + 1);
 			else
 				s.erase(n, s.size());
-			tmp = 1;
 		}
+	}
+
+	for (n = 0; n != -1;)
 		if ((n = s.find("@", 0)) != -1)
-		{
 			s.erase(n, ((m = s.find(".", n)) != -1) ? (m - n + 1) : s.size());
-			tmp = 1;
-		}
-	} while (tmp);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL, "Russian");
 
-	string s = "Тестовая строка.\n$<- Должна добавиться красная строка.\n#ТЕСТ <- А тут не должно быть слово ТЕСТ.\n@ТЕСТ ТЕСТ ТЕСТ.<-Тут не должно быть предложения ТЕСТ ТЕСТ ТЕСТ.";
+	string s = "$<- Должна добавиться красная строка.\n#ТЕСТ <- А тут не должно быть слово ТЕСТ.\n@ТЕСТ ТЕСТ ТЕСТ.<-Тут не должно быть предложения ТЕСТ ТЕСТ ТЕСТ.";
 	cout << s << endl;
 	replace(s);
 	cout << s << endl;
