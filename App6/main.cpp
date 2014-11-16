acbbb20fa4d6b00db091def6f41874412a0fe544
An2An <an2an96@bk.ru>
2014-11-16 22:18:31 +0300

    Update main.cpp

--- App6/main.cpp
+++ App6/main.cpp
@@ -6,28 +6,30 @@
 		+ ввод
 		+ вывод
 		+ копирование
-		- индексация по ключу
+		+ индексация по ключу
 		+ добавление элемента
 
 	Немиров Антон 13501/3 [https://github.com/An2An96/]
 */
 
-#include "main.h"
-#include "IDString_Class.cpp"
-
-using namespace std;
+#include "stdafx.h"
 
 int main()
 {
 	//	Init
 	setlocale(LC_ALL, "Russian");
-	srand(time(NULL));
 
 	//	Work
-	IDString stmp("Тестовая строка:)");
-	stmp.output();
-	stmp.input("Меняем строку... вот она уже другая!");
-	stmp.output();
+	assoc_array	arr(10);
+
+	arr.set(1000, "Строка с ключем 1000.");
+	arr.output(1000);
+
+	arr.set(1, "Строка с ключем 1.");
+	std::cout << arr[1];
+
+	arr.copy(1000, 1);
+	arr.output(1000);
 
 	system("pause");
 	return 0;
