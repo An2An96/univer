d2c5f5fbaa0e27dd053e5f9fe8812a6a51d1f4c0
An2An <an2an96@bk.ru>
2014-11-16 22:18:08 +0300

    Update and rename IDString_Class.cpp to array_class.cpp

---
+++ App6/array_class.cpp
@@ -0,0 +1,104 @@
+/*Немиров Антон 13501/3 [https://github.com/An2An96/]*/
+
+#include "stdafx.h"
+
+assoc_array::assoc_array(int size)
+{
+	_iUpperBound = size;
+	_iElements = (char**)calloc(_iUpperBound, sizeof(char*));
+	_iKeys = (int*)calloc(_iUpperBound, sizeof(int*));
+
+	for (int i = 0; i < _iUpperBound; i++)
+	{
+		_iElements[i] = NULL;
+		_iKeys[i] = -1;
+	}
+}
+
+assoc_array::~assoc_array()
+{
+	for (int i = 0; i < _iUpperBound; i++)
+		if (_iElements[i] != NULL)
+			free(_iElements[i]);
+	free(_iElements);
+	free(_iKeys);
+}
+
+bool assoc_array::output(int iKey)
+{
+	int iCell = findKey(iKey);
+	if (iCell == -1)
+	{
+		std::clog << "The cell with the same key is not set" << std::endl;
+		return false;
+	}
+	std::cout << _iElements[iCell];
+	return true;
+}
+
+bool assoc_array::set(int iKey, char* cString)
+{
+	int iCell = findKey(iKey);
+	if (iCell == -1)
+	{
+		if ((iCell = findFreeKey()) == -1)
+		{
+			std::clog << "No free cells" << std::endl;
+			return false;
+		}
+		_iKeys[iCell] = iKey;
+	}
+
+	//	if isset
+	if (_iElements[iCell] != NULL)
+		free(_iElements[iCell]);
+
+	int len = strlen(cString) + 1;
+	_iElements[iCell] = (char*)calloc(len, sizeof(char));
+	for (int i = 0; i < len; i++)
+		_iElements[iCell][i] = cString[i];
+	return true;
+}
+
+bool assoc_array::copy(int iDestKey, int iSourceKey)
+{
+	
+	int iSourceCell = findKey(iSourceKey);
+	if (iSourceCell == -1)
+	{
+		std::clog << "Source string is not set" << std::endl;
+		return false;
+	}
+	return assoc_array::set(iDestKey, _iElements[iSourceCell]);
+}
+
+char* assoc_array::operator[](int iKey)
+{
+	int iCell = findKey(iKey);
+	if (iCell == -1)
+	{
+		std::clog << "The cell with the same key is not set" << std::endl;
+		return NULL;
+	}
+	return _iElements[iCell];
+}
+
+int assoc_array::findKey(int iKey)
+{
+	for (int i = 0; i < _iUpperBound; i++)
+	{
+		if (_iKeys[i] == iKey)
+			return i;
+	}
+	return -1;
+}
+
+int assoc_array::findFreeKey()
+{
+	for (int i = 0; i < _iUpperBound; i++)
+	{
+		if (_iKeys[i] == -1)
+			return i;
+	}
+	return -1;
+}
